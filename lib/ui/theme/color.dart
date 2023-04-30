6a2a003435b04a79b965b43cf3faa6729f45a173
Asus <nishantbhardwaj2367@gmail.com>
2023-04-30 16:03:28 +0530

    first commit

---
+++ lib/ui/theme/color.dart
@@ -0,0 +1,8 @@
+//now let's add the color that we will need to project
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 30, 38, 44);
+  static Color secondaryColor = Color.fromARGB(255, 78, 100, 114);
+  static Color accentColor = Color.fromARGB(255, 194, 206, 213);
+}
